e2f0aea9250d3481c8fbf1ae20e718fdd7d090a0
Kelvin Muyambo <kelvinm@afrosoft.co.zw>
2018-10-02 12:51:35 +0200

    commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,37 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MultinavComponent } from './components/multinav/multinav.component';
+import { IndustriesComponent } from './components/industries/industries.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ServicesDetailsComponent } from './components/services-details/services-details.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CommunityComponent } from './components/community/community.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MultinavComponent
+    MultinavComponent,
+    IndustriesComponent,
+    ServicesComponent,
+    ServicesDetailsComponent,
+    ProductsComponent,
+    CommunityComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent }])
+      { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'industries', component: IndustriesComponent },
+      { path: 'services', component: ServicesComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'community', component: CommunityComponent },
+      { path: 'about', component: AboutComponent }     
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
